1d8e0d420491179a7ed8f75c9b7ed498307d827a
Ilhaam Akmal <ilhaam43@gmail.com>
2021-05-13 10:46:01 +0700

    add: swagger documentation to mini Node.js REST API

--- node-restapi-swagger/server.js
+++ node-restapi-swagger/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDb = require("./config/db");
 const { catchphrases } = require("./routes/index");
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
-connectDB();
+connectDb();
 
 app.use(express.json());
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Catchphrases REST API',
+            description: "A REST API built with Express and MongoDB. This API provides movie catchphrases and the context of the catchphrase in the movie."
+        },
+    },
+    apis: ["./routes/catchprases.js"]
+}
+
 app.use('/catchphrases', catchphrases)
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-});
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.listen(process.env.PORT || 5000, () => console.log('Up and running '));
+
+app.listen(process.env.PORT || 5000, () => console.log('Up and fly to the moon 🚀'));
